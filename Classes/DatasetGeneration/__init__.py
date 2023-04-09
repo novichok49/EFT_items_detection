749784323deb29094acdaa78a3c059256e35ccb8
Yaroslav Ageev <ageev.yaroslav.job@gmail.com>
2023-04-09 21:41:10 +0300

    Added APIRequester class

--- Classes/DatasetGeneration/__init__.py
+++ Classes/DatasetGeneration/__init__.py
@@ -2,3 +2,4 @@ from .ImagesGenerator import ImagesGenerator
 from .GridPacker import GridPacker
 from .ImagesDownloader import ImagesDownloader
 from .ImagesDataset import ImagesDataset
+from .APIRequester import APIRequester
