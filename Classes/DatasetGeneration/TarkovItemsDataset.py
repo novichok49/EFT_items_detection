3d84e5ecfb29d979bd63986d2f90526562722643
Yaroslav Ageev <ageev.yaroslav.job@gmail.com>
2023-07-02 21:33:23 +0300

    Refactored for new dataset format

--- Classes/DatasetGeneration/TarkovItemsDataset.py
+++ Classes/DatasetGeneration/TarkovItemsDataset.py
@@ -1,47 +1,37 @@
 from pathlib import Path
-from typing import Dict, List, Iterable
+from typing import Dict, List, Iterable, Tuple
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms as T
 from PIL import Image
 import json
-
+import yaml
 
 class TarkovItemsDataset(Dataset):
     def __init__(self,
-                 images_path: str | Path,
-                 labels_map: Dict = None,
-                 transforms=[T.ToTensor()]) -> None:
-        self.images_path = Path(images_path)
-        data = self.__try_load(self.images_path)
+                 dataset_path: str | Path,
+                #  labels_map: Dict = None,
+                 transforms=None) -> None:
+        self.dataset_path = Path(dataset_path)
         self.transforms = transforms
-        if labels_map != None:
-            self.labels_map = labels_map
-        else:
-            self.labels_map = data['labels_map']
-
-        self.images_map = data['images_map']
-        self.images = data['images']
 
     def __getitem__(self, index):
         target = {}
-        file_name = self.images_map[index]
-        image_path = self.images_path / file_name
-        image = Image.open(image_path).convert('RGB')
-        image_data = self.images[index]
-        transform = T.Compose(self.transforms)
-        im_w, im_h = image.size
-        image = transform(image)
-        t_w, t_h = image.shape[-1:-3:-1]
-        labels = torch.tensor(image_data['labels'], dtype=torch.int64)
-        boxes = torch.tensor(image_data['boxes'], dtype=torch.float16)
-        iscrowd = torch.zeros((boxes.shape[0],), dtype=torch.int8)
+        image_name = f'{index}.png'.zfill(10)
+        labels_name = f'{index}.txt'.zfill(10)
+        labels, boxes = self.read_bound_boxes(self.dataset_path / 'labels' / labels_name)
+        image = Image.open(self.dataset_path / 'images' / image_name).convert('RGB')
+        image = T.ToTensor()(image)
+        if self.transforms:
+            transform = T.Compose(self.transforms)
+            image = transform(image)
+        w, h = image.shape[2], image.shape[1]
+        labels = torch.tensor(labels, dtype=torch.int64)
+        boxes = torch.tensor(boxes, dtype=torch.float32)
+        boxes[::, 0::2] *= w
+        boxes[::, 1::2] *= h
+        iscrowd = torch.zeros((boxes.shape[0]), dtype=torch.int8)
         image_id = torch.tensor([index], dtype=torch.int64)
-        if not(im_w == t_w and im_h == t_h):
-            w_ratio = t_w / im_w
-            h_ratio = t_h / im_h
-            boxes[:, 0::2] *= w_ratio
-            boxes[:, 1::2] *= h_ratio
         target['boxes'] = boxes
         target['labels'] = labels
         target['area'] = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
@@ -50,63 +40,15 @@ class TarkovItemsDataset(Dataset):
         return image, target
 
     def __len__(self):
-        return len(self.images)
+        return len(list((self.dataset_path / 'images').glob('*')))
 
-    def __del__(self):
-        self.save()
+    def read_bound_boxes(self, labels_path: Path) -> Tuple[list, list]:
+        labels = []
+        boxes = []
+        with open(labels_path, 'r') as file:
+            for line in file:
+                one_line = line.strip().split(sep=' ')
+                labels.append(int(one_line[0]))
+                boxes.append([float(x) for x in one_line[1:]])
+        return labels, boxes
 
-    def add_image(self,
-                  image_name: str,
-                  bboxes: List,
-                  labels: List) -> None:
-        image_data = {'boxes': bboxes,
-                      'labels': labels}
-        id = len(self.images)
-        self.images_map[id] = image_name
-        self.images[id] = image_data
-
-    def decode_labels(self, ids: Iterable):
-        labels = [self.labels_map[int(id)]
-                  for id in ids]
-        return labels
-
-    def save(self) -> None:
-        data = {'images': self.images,
-                'images_map': self.images_map,
-                'labels_map': self.labels_map}
-        file_path = self.images_path / '.json'
-        with open(file_path, 'w') as file:
-            json.dump(data, file)
-
-    def get_image(self, index):
-        target = {}
-        file_name = self.images_map[index]
-        image_path = self.images_path / file_name
-        image = Image.open(image_path).convert('RGB')
-        image_data = self.images[index]
-        target['boxes'] = image_data['boxes']
-        target['labels'] = image_data['labels']
-        return image, target
-
-    def __try_load(self, path: Path) -> Dict[str, Dict]:
-        def parse_int_keys(key):
-            try:
-                res = int(key)
-            except:
-                res = key
-            return res
-
-        def hook(dct):
-            return {parse_int_keys(k): v for k, v in dct.items()}
-
-        file_path = path / '.json'
-        if file_path.exists():
-            with open(file_path, 'r') as file:
-                data = json.load(file, object_hook=hook)
-            return {'images': data['images'],
-                    'images_map': data['images_map'],
-                    'labels_map': data['labels_map']}
-        else:
-            return {'images': {},
-                    'images_map': {},
-                    'labels_map': {}}
